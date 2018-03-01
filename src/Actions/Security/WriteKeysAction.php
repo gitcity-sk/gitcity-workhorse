5ce37fd72a9370dfe5de866602940f9aa1d2aefe
Martin <35365937+martinkukolos@users.noreply.github.com>
2018-03-01 13:54:47 +0100

    Update WriteKeysAction.php

--- src/Actions/Security/WriteKeysAction.php
+++ src/Actions/Security/WriteKeysAction.php
@@ -31,18 +31,18 @@ class WriteKeysAction extends AbstractAction implements ActionInterface
         $keysString = '';
 
         foreach ($options->keys as $key => $value) {
-            $keysString .= 'command="/var/www/html/embeded/git-shell/ssh-exec key-' . $value->id . '",no-port-forwarding,no-x11-forwarding,no-agent-forwarding,no-pty ' . $value->public_key . "\n";
+            $keysString .= 'command="' . $options->ssh_exec . ' key-' . $value->id . '",no-port-forwarding,no-x11-forwarding,no-agent-forwarding,no-pty ' . $value->public_key . "\n";
         }
 
         $fs = new Filesystem();
         if (!$fs->exists($options->path)) $fs->mkdir($options->path, 0755);
 
-        Log::show('Writing authorized_keys for git user');
+        Log::show('Writing authorized_keys for git user to ' . $options->path);
         file_put_contents($options->path . 'authorized_keys', $keysString);
 
         // HADRCODED TOTO to change it
-        Log::show("chown -R 1000 /var/opt/gitcity/git-data/");
-        $process = new Process("chown -R 1000 /var/opt/gitcity/git-data/");
+        Log::show("chown -R git " . $options->path);
+        $process = new Process("chown -R git " . $options->path);
         $process->start();
         while ($process->isRunning()) {
             // waiting for process to finish
